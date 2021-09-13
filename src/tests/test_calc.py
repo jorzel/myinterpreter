ad655a7e3e24fc1ae36b35602fd9c2b561c94e3e
Jarosław Orzeł <jaroslaw.orzel@emplocity.pl>
2021-09-13 10:34:29 +0200

    Added nop test

--- src/tests/test_calc.py
+++ src/tests/test_calc.py
@@ -5,6 +5,18 @@ from src.calc import Interpreter
 
 @pytest.mark.parametrize(
     "expression,expected_result",
+    [("2", 2), ("12", 12)],
+)
+def test_calc_nop(expression, expected_result):
+    interpreter = Interpreter(expression)
+
+    result = interpreter.expr()
+
+    assert result == expected_result
+
+
+@pytest.mark.parametrize(
+    "expression,expected_result",
     [("1 +2", 3), ("1+2", 3), ("2+8", 10), ("12+3", 15), ("13+2", 15), ("18+12", 30)],
 )
 def test_calc_adding_two_integers(expression, expected_result):
